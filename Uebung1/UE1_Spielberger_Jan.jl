9eeb7dfd586d3d1e9cf72abe0274bda7d2bbe5b7
Jan Spielberger <jan.spielberger32@gmail.com>
2025-04-10 15:39:03 +0200

    Fertigstellung der Aufgabe 1

--- Uebung1/UE1_Spielberger_Jan.jl
+++ Uebung1/UE1_Spielberger_Jan.jl
@@ -8,7 +8,7 @@ Wichtig 2: Bitte nur die Funktionen ausprogrammieren und keine Funktionsaufrufe 
 # Die Bearbeitung der Aufgaben ist in einem Testskript erfolgt, der Code wurde von
 # diesen Testskript kopiert und hier in diesem Skript eingefügt und anschließend
 # wurden die jeweiligen "erwähnenswerten" commits auf Github gepushed. (Deshalb
-# die eher geringen Zeitabstände der commits.)
+# die eher geringen Zeitabstände der commits.). Zudem wurde der Ordner erneut hochgeladen
 
 ### Beispiel 1:
 
